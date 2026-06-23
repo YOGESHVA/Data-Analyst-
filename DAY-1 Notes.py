a0a622a84a499308a59e31a6fa6f79362b400a3a
YOGESHVA <yogeshvarma2901@gmail.com>
2026-06-23 22:49:01 +0530

    Add files via upload

---
+++ DAY-1 Notes.py
@@ -0,0 +1,122 @@
+'''
+Procedural language
+------------------
+---> This follows a step by step approach,where code is structure into procedure such as
+fuction or routines...  but python line by line execution
+eg ---> c
+object oriented :It based on concepts of object and class...
+--------
+--> #Example
+print(89+67) # BUt output is came it runtime it will be used
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+print (type(num))
+print(id(num)) # But It can be save and store and acceess the data by using id
+#If any create object that can be store permentaly
+#What is Python
+#1.Dynamically typed :python Knows the type of data we passing to the variable..
+num='yogesh'
+print(type(num))
+
+#2.Interperter:Python Excute code line-by-line,if any error it will stop the execution in that line and before will gives the output
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+print(total)
+print (type(num))'''
+       
+#3.High_level : It will be the taken low memory allocation we can not take, it will take high level
+
+#Python In Year 1991,Guide Van Russum
+
+"""Why use python
+-----------------
+--> easy syntax
+-->cross-platform:Wide platforms like node++,nodepad,linux,Windows
+-->wide application:Use in data science,Ai,Machine learning
+-->Huge library support:Pandas,Matplot lib
+
+Tokens:These words are reserved
+--------
+-->We have 5 Tokens
+1,Keywords:words are reserved
+Example:If,else,for,while These are reserved words in python
+-----------
+2,Identifires:Name given to varibales,functions,class
+
+3,Literals:It is nothing but Data Types,Right side vi Literals
+Example so ="hi", hi is called literals
+
+
+4,Operators:+,-*
+Example +,-,*
+5,Punctuators:Square,Curly,partences
+{},[],
+--------
+Variables:The value that can be store in data
+Rules of Variable
+-----------------
+Good
+1,It can be start with small letter,captial letter
+eg:
+num_1=7
+num=8
+Num_2 =9
+2,It should not start with Special char,number,space
+$num =0
+su@m = 8
+s o = 89
+2num =9
+
+
+Comments :
+1,Single line ---> #
+2,Multi line ---> ''',""","""
+Example
+num=9
+if num %2 == 0: # Checking a number is even or odd
+    print("Even")
+else:
+    print("Odd")
+
+Boolean : It tells about true or false"""
+num = 8
+num_2 = 8
+print(num == num_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
